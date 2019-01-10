780c2177389a6669706fa8a18391404d609d0341
Emil Craciun <emil.craciun@live.com>
2019-01-10 15:22:01 +0200

    Added ApplicationInsights and saved key to FunctionApp settings

--- templates/provision.ps1
+++ templates/provision.ps1
@@ -2,7 +2,8 @@ param (
     [string]$resourceGroup,
     [string]$location = 'westeurope',
     [string]$storageName,
-    [string]$functionName
+    [string]$functionAppName,
+    [string]$applicationInsightsName
 )
 
 # Creates a Resource group to hold our application
@@ -12,4 +13,13 @@ az group create -l $location -n $resourceGroup
 az storage account create --sku Standard_LRS --location $location --kind Storage -g $resourceGroup -n $storageName.ToLower() --https-only $true
 
 # Creates our Function Application
-az functionapp create -n $functionName -g $resourceGroup -s $storageName.ToLower() -c $location --os-type Windows --runtime dotnet
+az functionapp create -n $functionAppName -g $resourceGroup -s $storageName.ToLower() -c $location --os-type Windows --runtime dotnet
+
+# Create a new Application Insights resource
+az resource create --resource-group $resourceGroup --resource-type "Microsoft.Insights/components" --name $applicationInsightsName --location $location --properties '{\"Application_Type\":\"web\"}' 
+
+# Get instrumentation key
+$instrumentationKey = az resource show -g $resourceGroup -n $applicationInsightsName --resource-type "Microsoft.Insights/components" --query properties.InstrumentationKey
+
+# Update Function App settings
+az functionapp config appsettings set --name $functionAppName --resource-group $resourceGroup --settings "APPINSIGHTS_INSTRUMENTATIONKEY = $instrumentationKey"
